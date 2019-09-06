dc5290b3ae6a416df05f9c66fcbd164307c5644b
Ahmed <ahmed.max7586@gmail.com>
2019-09-06 13:01:08 -0400

    added method => auth-service.js

--- thingful-server/src/auth/auth-service.js
+++ thingful-server/src/auth/auth-service.js
@@ -2,8 +2,24 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+const xss = require('xss')
 
 const AuthService = {
+
+  hasUserWithUserName(db, user_name) {
+     return db('thingful_users')
+       .where({ user_name })
+       .first()
+       .then(user => !!user)
+   },
+
+   insertUser(db, newUser) {
+     return db
+       .insert(newUser)
+       .into('thingful_users')
+       .returning('*')
+       .then(([user]) => user)
+   },
 
   getUserWithUserName(db, user_name) {
     return db('thingful_users')
@@ -24,7 +40,19 @@ const AuthService = {
   
   verifyJwt(token) {
     return jwt.verify(token, config.JWT_SECRET, {algorithm: 'HS256'})
-  }
+  },
+     hashPassword(password) {
+   return bcrypt.hash(password, 12)
+ },
+   serializeUser(user) {
+     return {
+       id: user.id,
+       full_name: xss(user.full_name),
+       user_name: xss(user.user_name),
+       nickname: xss(user.nick_name),
+       date_created: new Date(user.date_created),
+     }
+   },
 };
 
 module.exports = AuthService;
